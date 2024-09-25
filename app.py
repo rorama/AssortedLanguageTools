d19d235eb90fa154a09d2daadd05d8ae75dd09c7
richard orama <richardorama@users.noreply.huggingface.co>
2024-09-25 08:28:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,18 @@ import ast
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # Default article text
-DEFAULT_ARTICLE = """ New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
-A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.
-Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.
-In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.
-Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the
-2010 marriage license application, according to court documents.
-"""
+# DEFAULT_ARTICLE = """ New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
+# A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.
+# Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.
+# In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.
+# Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the
+# 2010 marriage license application, according to court documents.
+# """
+
+DEFAULT_ARTICLE = ""
 
 # Create a text area for user input
-ARTICLE = st.sidebar.text_area('Enter Article', DEFAULT_ARTICLE, height=150)
+ARTICLE = st.sidebar.text_area('Enter Article (String)', DEFAULT_ARTICLE, height=150)
 
 # Define the summarization function
 def summarize(txt):
@@ -45,15 +47,17 @@ else:
 sentiment_pipeline = pipeline("sentiment-analysis")
 
 # Default article text
-DEFAULT_SENTIMENT = """[
-    "I'm so happy today!",
-    "This is the worst experience ever.",
-    "It's a decent product, nothing special."
-]"""
+# DEFAULT_SENTIMENT = """[
+#     "I'm so happy today!",
+#     "This is the worst experience ever.",
+#     "It's a decent product, nothing special."
+# ]"""
 
-DEFAULT_SENTIMENT = "I'm so happy today!"
+# DEFAULT_SENTIMENT = "I'm so happy today!"
+DEFAULT_SENTIMENT = ""
+
 # Create a text area for user input
-SENTIMENT = st.sidebar.text_area('Enter Sentiment', DEFAULT_SENTIMENT, height=150)
+SENTIMENT = st.sidebar.text_area('Enter Sentiment (String or List of Strings)', DEFAULT_SENTIMENT, height=150)
 
 def is_valid_list_string(string):
     try:
